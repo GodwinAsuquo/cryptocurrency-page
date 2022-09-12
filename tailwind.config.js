300bd6e1c1e93860913d8d6778a5d20620d60f88
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-09-12 01:15:37 +0100

    added images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+     screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        deepBlue: 'background: #0D0D2B',
+        lightBlue: 'background: #3671E9',
+        deepPurple: '#2B076E',
+        lightPurple: '#491F98',
+      }
+    },
   },
   plugins: [],
 }
